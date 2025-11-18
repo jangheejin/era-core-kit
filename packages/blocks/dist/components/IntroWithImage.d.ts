aa6129c01a6e7cd8f835416d092c5d0ed18abdfc
jangheejin <ejaneheejin@gmail.com>
2025-11-18 01:04:02 -0500

    local build

---
+++ packages/blocks/dist/components/IntroWithImage.d.ts
@@ -0,0 +1,3 @@
+import type { IntroWithImageProps } from '../types';
+export declare function IntroWithImage({ heading, text, imageUrl }: IntroWithImageProps): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=IntroWithImage.d.ts.map
---
+++ packages/blocks/dist/index.d.ts
@@ -0,0 +1,4 @@
+export * from './components';
+export * from './registry';
+export type { LayoutBlock, BlockComponentProps, GenericBlock } from './types';
+//# sourceMappingURL=index.d.ts.map
