9b5c1b6ccc90890df16edb7ffceb6dfe1ea306c9
jangheejin <ejaneheejin@gmail.com>
2025-11-17 23:20:32 -0500

    got rid of junk code in apps/site/app/page.tsx

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,28 +1,10 @@
+//apps/site/app/page.tsx
+
 'use client';
 import React from 'react';
 import { homeLayout } from '@/content/home.layout';
 //import { homeLayout } from '../src/content/home.layout';
 import BlockRenderer from '@/utils/BlockRenderer'; 
-//import { renderBlock } from '@/utils/renderBlock';
-/*
-export default function Home() {
-  return (
-    <main>
-      {homeLayout.map((block, index) => renderBlock(block, index))}
-    </main>
-  );
-}*/
-/*
-export default function Home() {
-  return <main>
-    {homeLayout.map((block, index) => (
-    // The key must be on the direct child of the map.
-      <React.Fragment key={index}> 
-        {renderBlock(block, index)}
-      </React.Fragment>
-    ))}
-  </main>;
-}*/
 
 const Home = () => {
   return (
