9ee08cb8e12f297e53729493a828efeaecac6a87
jangheejin <ejaneheejin@gmail.com>
2025-11-18 00:45:53 -0500

    re-added export * from ./components; to packages/blocks/src/index.ts ... for some reason i had commented it out earlier... still not really sure about this one.

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -1,6 +1,10 @@
 // packages/blocks/src/index.ts
+
 // Re-export layout components for use in apps
-//export * from './components';
+// This is the one typescript actually sees
+
+export * from './components';
 export * from './registry';
 /*export * from './types';*/
 export type { LayoutBlock, BlockComponentProps, GenericBlock } from './types';
+export { Callout } from './components/Callout';
