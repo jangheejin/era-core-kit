a6e61fa37e84cb78ba3db05ed22d5675f5ebd672
jangheejin <ejaneheejin@gmail.com>
2025-11-19 12:45:05 -0500

    changed import path for homeLayout  from ../src/content/home.layout to  @/content/home.layout as it is my understanding that the so-called explicit import paths are more prone to breaking in production envs

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -3,7 +3,6 @@
 "use client";
 import React from 'react';
 import { homeLayout } from '@/content/home.layout';
-//import { homeLayout } from '../src/content/home.layout';
 import BlockRenderer from '@/utils/BlockRenderer'; 
 
 const Home = () => {
