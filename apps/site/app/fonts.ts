7edead99cf345deb53a5c224f9da38f05a2d3d9c
jangheejin <ejaneheejin@gmail.com>
2026-01-31 17:03:55 -0500

    hardening fonts

---
+++ apps/site/app/fonts.ts
@@ -0,0 +1,27 @@
+//apps/site/app/fonts.ts
+
+import localFont from "next/font/local";
+
+export const inter = localFont({
+  src: [
+    {
+      path: "./fonts/inter-latin-wght-normal.woff2",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+export const oswald = localFont({
+  src: [
+    {
+      path: "./fonts/oswald-latin-wght-normal.woff2",
+      weight: "200 700",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-oswald",
+});
